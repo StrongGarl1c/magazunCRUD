0c9aedc3bbb08c978a28b247fa20450dbcf1e396
StrongGarl1c <jarkkocs16@rambler.ru>
2020-10-15 17:00:10 +0300

    створюю операцію Read

--- routes/read.js
+++ routes/read.js
@@ -5,25 +5,25 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 // eslint-disable-next-line no-undef
 const uri = process.env.API_URI;
-const client = new MongoClient(uri, { useUnifiedTopology: true });
 
 // read from database
 router.get('/', (req, res) => {
   // connect to db
   async function run() {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
     await client.connect();
     const database = client.db('magazunCRUD');
     const collection = database.collection('tovar');
 
     const dataArray = [];
-    const cursor = collection.find({});
+    const cursor = collection.find({}).sort({ _id: -1 });
     cursor.forEach(
       (element) => {
         dataArray.push(element);
       },
       () => {
-        res.json(dataArray);
         client.close();
+        res.render('read', { someData: dataArray });
       },
     );
   }
